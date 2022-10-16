dc29be2c537fbfec7fe2d741fd21dcdf40c14ed4
jigar pariyar <divacre71@gmail.com>
2022-10-16 14:00:21 +0530

    added main project

---
+++ precache-manifest.8eff8875823739cd1d99b1df374882dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2ff3cbb2b9154c6716",
+    "url": "/static/css/main.8f3d9680.chunk.css"
+  },
+  {
+    "revision": "6b2ff3cbb2b9154c6716",
+    "url": "/static/js/main.6b2ff3cb.chunk.js"
+  },
+  {
+    "revision": "52a192d8b6d7e13699b4",
+    "url": "/static/js/1.52a192d8.chunk.js"
+  },
+  {
+    "revision": "bec55280da4a78034af7",
+    "url": "/static/js/2.bec55280.chunk.js"
+  },
+  {
+    "revision": "1182a501a03ea3213fd1",
+    "url": "/static/js/runtime~main.1182a501.js"
+  },
+  {
+    "revision": "775deed927d9d27c504c842a0014ae6f",
+    "url": "/static/media/gettyimages-484107954-612x612.775deed9.jpg"
+  },
+  {
+    "revision": "04f784c807db372af402e180d3136ceb",
+    "url": "/static/media/pexels-krishna-studio-2293102.04f784c8.jpg"
+  },
+  {
+    "revision": "cb07b14562c3d601ad7e8567f9652e0c",
+    "url": "/static/media/SWAS6411.cb07b145.JPG"
+  },
+  {
+    "revision": "3bc777114eaea9a8dbe37507814e3699",
+    "url": "/static/media/gettyimages-583763498-612x612.3bc77711.jpg"
+  },
+  {
+    "revision": "f876b970232687bee3813861f67a30c3",
+    "url": "/static/media/SUNY1245.f876b970.jpg"
+  },
+  {
+    "revision": "1f33e023aa2eb4d2ad3d64b1bccf68c1",
+    "url": "/static/media/IMG_20200829_145319_934.1f33e023.jpg"
+  },
+  {
+    "revision": "12f305f4756588960005c8129fe2d572",
+    "url": "/static/media/koti.12f305f4.jpg"
+  },
+  {
+    "revision": "f485dbda88e28925afb76ff019063d1b",
+    "url": "/static/media/DSC00636.f485dbda.JPG"
+  },
+  {
+    "revision": "e00cacd6339eb3be4fbe0062c84aaeaf",
+    "url": "/static/media/SWAS8684.e00cacd6.JPG"
+  },
+  {
+    "revision": "6cb718af44cd0f24f8815954edb03753",
+    "url": "/static/media/indowestern.6cb718af.jpg"
+  },
+  {
+    "revision": "07480db6acbdcb2624bfe9798dd92785",
+    "url": "/index.html"
+  }
+];
